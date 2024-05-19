e2b3e5800afaab4b86b72a94cc4c52972b4eb330
Milan Lazić <90383105+mlanlazc@users.noreply.github.com>
2024-05-19 13:03:58 +0200

    liblab SDK update for version v3.3.1

--- src/main/java/io/github/mlanlazc/Configuration.java
+++ src/main/java/io/github/mlanlazc/Configuration.java
@@ -2,7 +2,7 @@ package io.github.mlanlazc;
 
 public class Configuration {
 
-  public static final String USER_AGENT = "Milanpetstoretest/3.3.0 Java/" + getJavaVersion();
+  public static final String USER_AGENT = "Milanpetstoretest/3.3.1 Java/" + getJavaVersion();
 
   public static final String DEFAULT_API_KEY_HEADER = "X-API-KEY";
 
